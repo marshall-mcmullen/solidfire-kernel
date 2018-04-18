8609c41c9861c9a90e4d79a126092073b9bca626
Sebastian Kuzminsky <seb@highlab.com>
2018-04-18 15:23:56 -0600

    Revert "audit.c"
    This reverts commit 5bcb2d626bd21283d6333c3227e2bfdc0f4c0757.

--- kernel/audit.c
+++ kernel/audit.c
@@ -2342,12 +2342,11 @@ void audit_log(struct audit_context *ctx, gfp_t gfp_mask, int type,
  * audit_log_secctx - Converts and logs SELinux context
  * @ab: audit_buffer
  * @secid: security number
- * 
+ *
  * This is a helper function that calls security_secid_to_secctx to convert
  * secid to secctx and then adds the (converted) SELinux context to the audit
  * log by calling audit_log_format, thus also preventing leak of internal secid
  * to userspace. If secid cannot be converted audit_panic is called.
- * 30a3ee52488fa5063e40c2481afc735ef482c7d4
  */
 void audit_log_secctx(struct audit_buffer *ab, u32 secid)
 {
